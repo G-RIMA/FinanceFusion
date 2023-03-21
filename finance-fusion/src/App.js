d6f102cfdf2c7d4cb97363e027b63087f21c085a
g-rima <gikandimaria178@gmail.com>
2023-03-21 20:04:35 +0300

    Creating Landing Page

--- finance-fusion/src/App.js
+++ finance-fusion/src/App.js
@@ -1,17 +1,15 @@
 import './App.css';
 import React from "react";
-import Balancesheet from './balancesheet';
-import IncomeStatement from './incomestatement';
-import CashFLow from './cashflow';
+//import Balancesheet from './balancesheet';
+//import IncomeStatement from './incomestatement';
+//import CashFLow from './cashflow';
+import './App.css';
+
 
 function App() {
   return (
     <div>
-        < Balancesheet />
-        <IncomeStatement />
-        <CashFLow />
-
-       
+      
     </div>
   );
 }
