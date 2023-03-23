d3fd3ca18ee152e08c72897d9d4f17c63ce9687c
g-rima <gikandimaria178@gmail.com>
2023-03-23 12:03:01 +0300

    Added article sections

--- finance-fusion/src/components/article/Article.jsx
+++ finance-fusion/src/components/article/Article.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 import './article.css';
 
 
-const Article = () => {
+const Article = ({ date, text }) => {
   return (
-    <div>
-      Article
+    <div className="ff__blog-container_article">
+    <div className="ff__blog-container_article-content">
+      <div>
+        <p>{date}</p>
+        <h3>{text}</h3>
+      </div>
+      <p>Read Full Article</p>
     </div>
-  )
-}
+  </div>
+);
+
+};
 
 export default Article;
--- finance-fusion/src/containers/blog/Blog.jsx
+++ finance-fusion/src/containers/blog/Blog.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import Article from '../../components/article/Article';
 import './blog.css';
 
 const Blog = () => {
   return (
-    <div>
-      Blog
+    <div className="ff__blog section__padding" id="blog">
+    <div className="ff__blog-heading">
+      <h1 className="gradient__text__features">A lot is happening, <br /> We are blogging about it.</h1>
     </div>
-  )
-}
+    <div className="ff__blog-container">
+      <div className="ff__blog-container_groupA">
+        <Article date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+      </div>
+      <div className="ff__blog-container_groupB">
+        <Article  date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+        <Article date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+        <Article date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+        <Article date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+      </div>
+    </div>
+  </div>
+);
+};
 
 export default Blog;
